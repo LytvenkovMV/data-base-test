ec0946c0233db064544f87a07763b4c90efe3ff8
Home PC <example@mail.com>
2025-03-13 14:24:25 +0300

    Disabled test

--- src/test/java/DataBaseTest.java
+++ src/test/java/DataBaseTest.java
@@ -1,6 +1,7 @@
 import org.example.Main;
 import org.example.repository.EmployeeRepository;
 import org.example.util.EmployeeUtil;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +17,7 @@ public class DataBaseTest {
     EmployeeRepository repo;
 
     @Test
+    @Disabled
     void insertEmployees() throws InterruptedException {
         long recordsNum = 50_000_000;
         int threadsNum = 10;
